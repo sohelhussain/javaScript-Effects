da126ed506a117f6de362fc9bf7824a31130eec9
sohelhussain <sohelhussaing@gmail.com>
2024-02-07 15:18:41 -0800

    making a awosome mouse move and image popup

---
+++ hyperplexed/mouse/app.js
@@ -0,0 +1,21 @@
+const images = document.getElementsByClassName(`image`)
+let globalIndex = 0;
+    last = {x: 0, y: 0}
+const activate = (image, x, y) => {
+    image.style.left = `${x}px`;
+    image.style.top = `${y}px`;
+    image.dataset.status = 'active'; 
+    last = {x, y}
+} 
+const distanstFromLast = (x, y) => {
+    return Math.hypot(x - last.x, y - last.y)
+}
+window.onmousemove = e => {
+    if(distanstFromLast(e.clientX, e.clientY) > 100){
+    const lead = images[globalIndex % images.length];
+          tail = images[(globalIndex - 5) % images.length];
+    activate(lead, e.clientX, e.clientY);
+    if(tail) tail.dataset.status = "inactive";
+    globalIndex++;
+}
+}
