1128c2bb476c6233d1db9deb11df37900576beb6
sohelhussain <sohelhussaing@gmail.com>
2024-03-23 23:08:17 +0530

    chack is variable is array are not

---
+++ tips/varisArray.js
@@ -0,0 +1,6 @@
+let city = 'New York';
+let cities = ['New York', 'Ujerezah', 'Maine', 'Orreob']
+
+const isArray = arr => Array.isArray(arr);
+console.log(isArray(city)); // false 
+console.log(isArray(cities)); // true 
